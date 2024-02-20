8d2b7655058eaaa45fec8fc307e711313ea0bcaa
optitudes <optitudes@localhost.localdomain>
2024-02-20 13:15:42 -0500

    added importations and filter modals, fixed some bugs on map.js

--- mapamundi-main/webapp/controllers/exportController.js
+++ mapamundi-main/webapp/controllers/exportController.js
@@ -1,5 +1,7 @@
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const conexion = require('../database/db');
+const fs = require('fs');
+const dir = 'downloads/';
 
 // Metodo para exportar a CSV
 exports.exportar = (req, res) => {
@@ -10,6 +12,17 @@ exports.exportar = (req, res) => {
                 res.status(500).send('Error interno del servidor');
                 return;
             }
+            //crea la carpeta  do'nde se almacenara' la exportacion (servidor)
+            if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir, { recursive: true });
+        }
+
+        // Crear el archivo si no existe, 
+        const filePath = `${dir}/egresadosdata.csv`;
+
+        if (!fs.existsSync(filePath)){
+            fs.writeFileSync(filePath, '');
+        }
 
             const csvWriter = createCsvWriter({
                 path: 'downloads/egresadosdata.csv',
@@ -30,3 +43,14 @@ exports.exportar = (req, res) => {
         res.status(500).send('Error interno del servidor');
     }
 }
+// Metodo para descargar la plantilla de egresados a CSV
+exports.downloadTemplate = (req, res) => {
+    try {
+        // Crear el archivo si no existe, 
+        res.download('downloads/importacionDeEgresadosTemplate.ods');
+        } catch (error) {
+        console.error('Error al descargar la plantilla :', error);
+        res.status(500).send('Error interno del servidor');
+    }
+}
+
